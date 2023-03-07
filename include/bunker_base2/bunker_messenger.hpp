8f078b50a31d9d4cbd2a2310562985d4b487c97d
Austin Greisman <austin.greisman@indrorobotics.com>
2023-03-07 10:53:42 -0500

    test update

--- include/bunker_base2/bunker_messenger.hpp
+++ include/bunker_base2/bunker_messenger.hpp
@@ -82,20 +82,23 @@ class BunkerMessenger {
     // Min battery voltage is 40V if battery voltage is above 30 V
     battery_msg.voltage = state.system_state.battery_voltage; 
     if (battery_msg.voltage < 35) {
-      battery_msg.percentage = 3.5363083052703092e-003 * pow(battery_msg.voltage, 4) 
+      float tmp_percentage = 3.5363083052703092e-003 * pow(battery_msg.voltage, 4) 
         -6.0604460460754128e-001 * pow(battery_msg.voltage, 3) +  
         3.7954435576128162e+001 * pow(battery_msg.voltage, 2) -
         1.0167870628390317e+003 * battery_msg.voltage + 
         9.6926828595033203e+003; //created from https://arachnoid.com/polysolve/
+      battery_msg.percentage = 
       // Ensure percentage is between 0 and 100
       if (battery_msg.percentage < 0) {
         battery_msg.percentage = 0;
       } else if (battery_msg.percentage > 100) {
         battery_msg.percentage = 100;
+      } else {
+        battery_msg.percentage = tmp_percentage;
       }
       battery_msg.design_capacity = 30; // 30Ah
     } else {
-      battery_msg.percentage = 6.5085267771558497e-001 * pow(battery_msg.voltage, 4) - 
+      float tmp_percentage = = 6.5085267771558497e-001 * pow(battery_msg.voltage, 4) - 
         6.8168813766265387e+001 * pow(battery_msg.voltage, 3) + 
         2.6690893112435269e+003*pow(battery_msg.voltage, 2) -
         4.6282892652307622e+004 * battery_msg.voltage + 
@@ -106,6 +109,8 @@ class BunkerMessenger {
         battery_msg.percentage = 0;
       } else if (battery_msg.percentage > 100) {
         battery_msg.percentage = 100;
+      } else {
+        battery_msg.percentage = tmp_percentage;
       }
     }
 
